5da33392272c9cb925a2c7435151d74020e24480
SebastianBinke <sebastian.binke@gmail.com>
2024-12-05 21:33:26 +0100

    burned out, had to watch a tutorial
    uni is too stressful right now

--- day5/result.py
+++ day5/result.py
@@ -1,2 +1,24 @@
-with open('/Users/sebastianbinke/Documents/GitHub/AdventOfCode/day5/input.txt', 'r', encoding='utf-8') as file:
-    data = file.read()
+
+def formatInput(input): # set of rules pre and post from input 
+  pairs = set((pre,post) for pre, post in 
+                      [map(int, line.strip().split('|')) for line in input if '|' in line])
+  updates = [list(map(int, line.strip().split(','))) for line in input if ',' in line] # updates 
+  return pairs, updates
+
+def findCorrect(updates):
+  return [u for u in updates if u == correctUpdate(u)]
+
+def findIncorrect(updates):
+  return [u for u in updates if u != correctUpdate(u)]
+
+def correctUpdate(u):
+  return sorted(u, key=lambda x: sum([(e,x) in pairs for e in u]))
+
+
+with open('/Users/sebastianbinke/Documents/GitHub/AdventOfCode/day5/input.txt', 'r') as file:
+    input = file.readlines()
+
+pairs, updates = formatInput(input)
+
+print('Part1: ', sum([u[len(u)//2] for u in findCorrect(updates)]))
+print('Part2: ', sum([u[len(u)//2] for u in map(correctUpdate, findIncorrect(updates))]))
